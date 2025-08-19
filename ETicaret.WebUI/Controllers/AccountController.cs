8fbe12da4d42700a3eb5f7eb3345d35366a5c8df
Enes Ilbay <enes@example.com>
2025-08-19 11:25:16 +0300

    Login Sayfası Eklendi

--- ETicaret.WebUI/Controllers/AccountController.cs
+++ ETicaret.WebUI/Controllers/AccountController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ETicaret.Core.Entities;
+using ETicaret.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ETicaret.WebUI.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly DataBaseContext _context;
+
+        public AccountController(DataBaseContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -12,9 +20,28 @@ namespace ETicaret.WebUI.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult SignIn(AppUser appUser)//giriş
+        {
+            return View();
+        }
         public IActionResult SignUp()//kayıt
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> SignUpAsync(AppUser appUser)//kayıt
+        {
+            appUser.IsAdmin = false;
+            appUser.IsActive = true;
+            if (ModelState.IsValid)
+            {
+                await _context.AddAsync(appUser);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(appUser);
+        
+    }
     }
 }
