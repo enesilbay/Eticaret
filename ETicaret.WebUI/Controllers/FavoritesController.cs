130e95593671215fade2bc303c4e6722ba7e4bcf
Enes Ilbay <enes@example.com>
2025-08-24 18:32:29 +0300

    Session'dan favori ürünleri alıp görüntüleyen FavoritesController oluşturuldu

---
+++ ETicaret.WebUI/Controllers/FavoritesController.cs
@@ -0,0 +1,22 @@
+﻿using ETicaret.Core.Entities;
+using ETicaret.WebUI.ExtensionMethods;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaret.WebUI.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var favoriler = GetFavorites();  //GetFavorites metodunu çağırarak gelen listeyi favoriler e atar ve onu da view'a gönderir.
+            return View(favoriler);
+        }
+
+        private List<Product> GetFavorites()
+        {
+            return HttpContext.Session.GetJson<List<Product>>("GetFavorites") ?? [];
+        }
+
+
+    }
+}
