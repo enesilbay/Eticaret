ef80db32eb028ef070e1cec0f923c612b7842f0a
Enes Ilbay <enes@example.com>
2025-08-24 18:58:34 +0300

    Add metoduyla favori ürün ekleme işlemi yapıldı, session entegrasyonu sağlandı

--- ETicaret.WebUI/Controllers/FavoritesController.cs
+++ ETicaret.WebUI/Controllers/FavoritesController.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaret.Core.Entities;
+using ETicaret.Data;
 using ETicaret.WebUI.ExtensionMethods;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,6 +7,14 @@ namespace ETicaret.WebUI.Controllers
 {
     public class FavoritesController : Controller
     {
+
+        private readonly DataBaseContext _context;
+        public FavoritesController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+
         public IActionResult Index()
         {
             var favoriler = GetFavorites();  //GetFavorites metodunu çağırarak gelen listeyi favoriler e atar ve onu da view'a gönderir.
@@ -15,6 +24,18 @@ namespace ETicaret.WebUI.Controllers
         private List<Product> GetFavorites()
         {
             return HttpContext.Session.GetJson<List<Product>>("GetFavorites") ?? [];
+        }
+
+        public IActionResult Add(int ProductId)
+        {
+            var favoriler = GetFavorites();
+            var product = _context.Products.Find(ProductId);
+            if (product != null && !favoriler.Any(p=>p.Id == ProductId)) // favoriler de bu ID ye eşit bir ürün içermiyorsa
+            {
+                favoriler.Add(product);
+                HttpContext.Session.SetJson("GetFavorites",favoriler);
+            }
+            return RedirectToAction("Index");
         }
 
 
