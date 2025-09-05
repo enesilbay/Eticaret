510ee7fa9cd158038b73545043d71922e4538d93
Enes Ilbay <enes@example.com>
2025-09-06 00:48:15 +0300

    iyzico sepet ögeleri dinamikleştirilerek iyzico entegrasyonu tamamlandı ve test edildi

--- ETicaret.WebUI/Controllers/CartController.cs
+++ ETicaret.WebUI/Controllers/CartController.cs
@@ -157,16 +157,7 @@ namespace ETicaret.WebUI.Controllers
                 OrderLines = []
             };
 
-            foreach (var item in cart.CartLines)
-            {
-                siparis.OrderLines.Add(new OrderLine()
-                {
-                    ProductId=item.Product.Id,
-                    Quantity=item.Quantity,
-                    OrderId=siparis.Id,
-                    UnitPrice=item.Product.Price,
-                });
-            }
+            
             #region OdemeIslemi
             Options options = new Options();
             options.ApiKey = _configuration["IyzicOptions:ApiKey"];
@@ -176,8 +167,8 @@ namespace ETicaret.WebUI.Controllers
             CreatePaymentRequest request = new CreatePaymentRequest();
             request.Locale = Locale.TR.ToString();
             request.ConversationId = HttpContext.Session.Id;
-            request.Price = siparis.TotalPrice.ToString();
-            request.PaidPrice = siparis.TotalPrice.ToString();
+            request.Price = siparis.TotalPrice.ToString().Replace(",",".");
+            request.PaidPrice = siparis.TotalPrice.ToString().Replace(",", ".");
             request.Currency = Currency.TRY.ToString();
             request.Installment = 1;
             request.BasketId = "B"+ HttpContext.Session.Id;
@@ -200,8 +191,8 @@ namespace ETicaret.WebUI.Controllers
             buyer.GsmNumber = appUser.Phone;
             buyer.Email = appUser.Email;
             buyer.IdentityNumber = "11111111111";
-            buyer.LastLoginDate = DateTime.Now.ToString(); //"2015-10-05 12:43:35";
-            buyer.RegistrationDate = appUser.CreateDate.ToString(); //"2013-04-21 15:12:09";
+            buyer.LastLoginDate = DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss"); //"2015-10-05 12:43:35";
+            buyer.RegistrationDate = appUser.CreateDate.ToString("yyyy-mm-dd hh:mm:ss"); //"2013-04-21 15:12:09";
             buyer.RegistrationAddress = siparis.DeliveryAddress; //"Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
             buyer.Ip = HttpContext.Connection.RemoteIpAddress?.ToString(); //"85.34.78.112";
             buyer.City = teslimatAdresi.City; //"Istanbul";
@@ -226,32 +217,37 @@ namespace ETicaret.WebUI.Controllers
             request.BillingAddress = billingAddress;
 
             List<BasketItem> basketItems = new List<BasketItem>();
-            BasketItem firstBasketItem = new BasketItem();
-            firstBasketItem.Id = "BI101";
-            firstBasketItem.Name = "Binocular";
-            firstBasketItem.Category1 = "Collectibles";
-            firstBasketItem.Category2 = "Accessories";
-            firstBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
-            firstBasketItem.Price = "0.3";
-            basketItems.Add(firstBasketItem);
 
-            BasketItem secondBasketItem = new BasketItem();
-            secondBasketItem.Id = "BI102";
-            secondBasketItem.Name = "Game code";
-            secondBasketItem.Category1 = "Game";
-            secondBasketItem.Category2 = "Online Game Items";
-            secondBasketItem.ItemType = BasketItemType.VIRTUAL.ToString();
-            secondBasketItem.Price = "0.5";
-            basketItems.Add(secondBasketItem);
+            //BasketItem firstBasketItem = new BasketItem();
+            //firstBasketItem.Id = "BI101";
+            //firstBasketItem.Name = "Binocular";
+            //firstBasketItem.Category1 = "Collectibles";
+            //firstBasketItem.Category2 = "Accessories";
+            //firstBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+            //firstBasketItem.Price = "0.3";
+            //basketItems.Add(firstBasketItem);
 
-            BasketItem thirdBasketItem = new BasketItem();
-            thirdBasketItem.Id = "BI103";
-            thirdBasketItem.Name = "Usb";
-            thirdBasketItem.Category1 = "Electronics";
-            thirdBasketItem.Category2 = "Usb / Cable";
-            thirdBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
-            thirdBasketItem.Price = "0.2";
-            basketItems.Add(thirdBasketItem);
+            foreach (var item in cart.CartLines)
+            {
+                siparis.OrderLines.Add(new OrderLine()
+                {
+                    ProductId = item.Product.Id,
+                    Quantity = item.Quantity,
+                    OrderId = siparis.Id,
+                    UnitPrice = item.Product.Price,
+                });
+
+                basketItems.Add( new BasketItem
+                {
+                    Id = item.Product.Id.ToString(),
+                    Name = item.Product.Name,
+                    Category1 = "Collectibles",
+                    ItemType = BasketItemType.PHYSICAL.ToString(),
+                    Price = (item.Product.Price).ToString().Replace(",",".")
+                });
+            }
+
+
             request.BasketItems = basketItems;
 
             Payment payment =await Payment.Create(request, options);
@@ -273,14 +269,16 @@ namespace ETicaret.WebUI.Controllers
                 }
                 else
                 {
-                    TempData["Message"] = "Ödeme işlemi gerçekleştirilemedi.Lütfen kart bilgilerinizi kontrol ediniz.";
+                    TempData["Message"] = "<div class='alert alert-danger'>" +
+                        "Ödeme işlemi gerçekleştirilemedi.Lütfen kart bilgilerinizi kontrol ediniz.</div>";
                 }
                 
             }
             catch (Exception)
             {
 
-               TempData["Message"] = "Siparişiniz alınırken bir hata oluştu.Lütfen tekrar deneyiniz.";
+               TempData["Message"] = "\"<div class='alert alert-danger'>\"" +
+                    "Siparişiniz alınırken bir hata oluştu.Lütfen tekrar deneyiniz.</div>";
             }
 
             return View(model);
